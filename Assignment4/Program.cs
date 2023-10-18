6ff24e8a1a29ef8920caa4e572d72f852cba0153
Chirukaru36 <kpoojashree470@gmail.com>
2023-10-18 20:14:23 +0530

    Add project files.

---
+++ Assignment4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringOperationLibrary;
+
+namespace Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringOperation stringOperation = new StringOperation();
+
+            
+            stringOperation.OnStringOperation += StringOperationHandler;
+
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            stringOperation.MakeFirstCharacterUppercase(input);
+        }
+
+        
+        static void StringOperationHandler(string input, string result)
+        {
+            Console.WriteLine($"Original string: {input}");
+            Console.WriteLine($"Resulting string: {result}");
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
---
+++ StringOperationLibrary/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringOperationLibrary
+{
+    public class StringOperation
+    {
+        public delegate void StringOperationHandler(string input, string result);
+
+        
+        public event StringOperationHandler OnStringOperation;
+
+        public void MakeFirstCharacterUppercase(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                OnStringOperation?.Invoke(input, "Input string is empty.");
+                return;
+            }
+
+            char[] charArray = input.ToCharArray();
+            if (char.IsLower(charArray[0]))
+            {
+                charArray[0] = char.ToUpper(charArray[0]);
+                string result = new string(charArray);
+                OnStringOperation?.Invoke(input, result);
+            }
+            else
+            {
+                OnStringOperation?.Invoke(input, "First character is already uppercase.");
+            }
+        }
+    }
+}
